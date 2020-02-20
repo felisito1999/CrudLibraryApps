961816718d2de4206072b4c9c0fd0574b61320ea
felisito1999 <60663860+felisito1999@users.noreply.github.com>
2020-02-19 21:54:28 -0400

    changes to EstadisticasJugadores Class

--- CrudAdvancedProgramming/ProjectData/ProjectData/DataMethods/EstadisticasJugadorMethods.cs
+++ CrudAdvancedProgramming/ProjectData/ProjectData/DataMethods/EstadisticasJugadorMethods.cs
@@ -10,31 +10,52 @@ namespace ProjectData.DataMethods
 {
     public class EstadisticasJugadorMethods
     {
-        public void SelectEstadisticasById(int id)
+        public EstadisticasJugador SelectEstadisticasByIdJugador(int idJugador)
         {
             var connection = ConnectionFactory.GetConnection();
             string selectEstadisticasByIdCommand = "SELECT CodigoStats, CodigoJugador, PuntosTotales, AsistenciasTotales, RebotesTotales, TirosDeCampoIntentados, " +
-                "TirosDeCampoEncestados, PerdidasDeBalon, JuegosTotales, JuegosIniciados, JuegosTotales, JuegosIniciados, FechaCreacion, Deleted, FROM EstadisticasJugador;";
+                "TirosDeCampoEncestados, PerdidasDeBalon, JuegosTotales, JuegosIniciados, FechaCreacion, Deleted, FROM EstadisticasJugador" +
+                "WHERE CodigoJugador = @id AND Deleted = 0;";
 
             using (connection)
             {
                 using (SqlCommand command = new SqlCommand(selectEstadisticasByIdCommand, connection))
                 {
+                    command.Parameters.AddWithValue("@id", idJugador);
                     connection.Open();
                     
                     SqlDataReader reader = command.ExecuteReader();
-                    List<EstadisticasJugador> estadisticasJugadorList = new List<EstadisticasJugador>();
+                    EstadisticasJugador estadisticasJugador = new EstadisticasJugador();
 
                     if (reader.HasRows)
                     {
                         while(reader.Read())
                         {
-                            EstadisticasJugador estadisticasJugador = new EstadisticasJugador();
+                            Jugador jugador = new Jugador();
+                            jugador.CodigoJugador = reader.GetInt32(1);
+                    
                             estadisticasJugador.CodigoStats = reader.GetInt32(0);
+                            estadisticasJugador.Jugador = jugador;
+                            estadisticasJugador.PuntosTotales = reader.GetInt32(2);
+                            estadisticasJugador.AsistenciasTotales = reader.GetInt32(3);
+                            estadisticasJugador.RebotesTotales = reader.GetInt32(4);
+                            estadisticasJugador.TirosDeCampoIntentados = reader.GetInt32(5);
+                            estadisticasJugador.TirosDeCampoEncestados = reader.GetInt32(6);
+                            estadisticasJugador.PerdidasDeBalon = reader.GetInt32(7);
+                            estadisticasJugador.JuegosTotales = reader.GetInt32(8);
+                            estadisticasJugador.JuegosIniciados = reader.GetInt32(9);
                         }
+                        connection.Close();
                     }
+                    return estadisticasJugador;
                 }
             }
+        }
+        public void InsertEstadisticasJugador(EstadisticasJugador estadisticas, Jugador jugador)
+        {
+            var connection = ConnectionFactory.GetConnection();
+            string insertEstadisticasCommand = "INSERT INTO EstadisticasJugador VALUES (@CodigoJugador, @PuntosTotales, @AsistenciasTotales, @RebotesTotales, @TirosDeCampoIntentados, " +
+                "@TirosDeCampoEncestadosyokjnb );"
         }
     }
 }
